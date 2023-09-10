4dfca21fce4219b4249929eda098c3016ecddfa3
Tyler Wasick <tyler@wasick.net>
2023-09-10 00:55:41 +0000

    Updated configuration wizard

--- models/config.py
+++ models/config.py
@@ -24,21 +24,24 @@ import connectionInfo
 
 ## Variables
 config                          = configparser.ConfigParser()
-connectionType                  = ["smb", "s3", "scp", "local"]
+connectionType                  = ["local", "smb", "s3", "scp"]
 mediaSettings                   = {
-    "movieSource"                   : "",
-    "moviesDestination"             : "",
-    "otherDestination"              : "",
+    "movieMKVSource"                : "",
+    "movieEncodedDestination"       : "",
+    "movieTempDestination"          : "",
+    "otherEncodedDestination"       : "",
     "otherSource"                   : "",
+    "otherTempLocation"             : "",
     "plexMovies"                    : "",
-    "plexTV"                        : "",
+    "plexTv"                        : "",
+    "tvEncodedDestination"          : "",
+    "tvSource"                      : "",
+    "tvTempLocation"                : "",
     "remoteConnectionType"          : "local",
-    "sourceConnectionType"          : "local",
-    "tvDestination"                 : "",
-    "tvSource"                      : ""
+    "sourceConnectionType"          : "local"
 }
 handbrakeSettings               = {
-    "handBrakeProfile"              : " --preset-import-gui Plex-HD.json --crop-mode none"
+    "handBrakeProfile"              : "--preset-import-gui Plex-HD.json --crop-mode none"
 }
 
 
@@ -86,6 +89,10 @@ class handbrake:
 
 ## Functions
 def loadConfig(configFile) -> bool:
+
+    # Local variables
+    validConfig                 = True
+    
     # Check if the config file exists
     if os.path.isfile(configFile):
         print("Config file exists: " + configFile)
@@ -111,13 +118,71 @@ def loadConfig(configFile) -> bool:
     # Read the config file
     config.read(configFile)
 
-    # Verify the config file has the required values
-    
+    # Verify media-settings in config file
+    for key in config["media-settings"]:
+        # If the value is empty, set the config validation variable to False 
+        if not config["media-settings"][key]:
+            validConfig = False
 
+    # If there are values missing in the config file, initiate the setup process
+    if not validConfig:
         
+        # Prompt the user for Plex connection information
+        inputMessage        = "Please select the method of which the script will be using to upload files your Plex server:\n"
+        userInput           = ""
+
+        for index, item in enumerate(connectionType):
+            inputMessage += f'{index+1}) {item}\n'
+
+        inputMessage += 'Please select: '
+
+        while (userInput not in "1"):
+            userInput = input(inputMessage)
+            if userInput.isdigit():
+                print('Plex connection set to: ' + connectionType[int(userInput) - 1]) #dev
+            
+            else:
+                print('Plex connection set to: ' + userInput) #dev
+        
+        # Prompt the user for the source type for media that is to be encoded
+
+        # If the source is "local", set temp locations to "local"
+
+        # Else if source is "SMB", prompt the user for the SMB connection information
+
+        # Else if source is "S3", prompt the user for the S3 connection information
+
+        # Else if source is "SCP", prompt the user for the SCP connection information
+
+        # TODO: Validate the connection
+
         # Write the changes to the config file
 
+    # Verify handbrake in config file
+    if config["handbrake"]["handBrakeProfile"] == "":
+        # Prompt user if they wish to use the default options, or set a profile
+        result = input("Handbrake profile not set. Would you like to use the default profile? (Y/n)")
+        
+        # If the user responded with "y", "Y", or a return value, use the default profile
+        if result == "" or result == "Y" or result == "y":
+            print("Using default profile")
+            config["handbrake"]["handBrakeProfile"] = "--preset-import-gui Plex-HD.json --crop-mode none"
+        
+        # Else, prompt the user to enter a profile
+        else:
+            print("Please enter a handbrake profile")
+            config["handbrake"]["handBrakeProfile"] = input("Handbrake profile: ")
+
+        # Write the changes to the config file
+        with open(configFile, "w") as file:
+            config.write(file)
+
         # Verify the changes were written successfully
+        if os.path.isfile(configFile):
+            print("Config file updated successfully")
+        else:
+            print("Failed to update config file. Aborting!")
+            sys.exit()
 
 ## Main entry point
 if __name__ == "__main__":
